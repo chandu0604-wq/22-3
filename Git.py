114c0933d0eeb85f673ddf9347a4c0295729fad2
Hruthik D Gowda <chandud961@gmail.com>
2026-03-22 02:00:13 +0530

    Add files via upload

---
+++ Git.py
@@ -0,0 +1,72 @@
+# class B:
+#     x = 5
+
+#     @classmethod
+#     def show(self):
+#         print(self.x)
+# B.show()
+
+
+# class Company:
+#     company_name = "Google"
+#     @classmethod
+#     def hi(cls):
+#         print(cls.company_name)
+
+# Company.hi()
+
+# class Math:
+
+#     @staticmethod
+#     def add(a,b):
+#         print(a+b)
+
+# Math.add(3,4)
+
+
+
+# class Test:
+
+#     @staticmethod
+#     def show(self):
+#         print("self")
+
+
+# class check:
+#     @staticmethod
+#     def hi():
+#         a = int(input("Enter the value: "))
+#         if a%2==0:
+#             print("Even")
+#         else:
+#             print("Odd")
+# check.hi()
+
+
+
+# class Product:
+#     count = 0
+#     def __init__(self,name,price):
+#         self.name=name
+#         self.price=price
+#         Product.count+=1
+    
+    
+#     def clss(self):
+#         print(f"the name of product is {self.name} and the price is {self.price}")
+        
+        
+#     @classmethod
+#     def helo(cls):
+#         print(f"count is {cls.count}")
+        
+#     @staticmethod
+    
+#     def hii(price,discount):
+#         print(f"the discount value is {price-(price*discount/100)}")
+    
+# r1 = Product("sabja", 100)
+# r2 = Product("sabja", 1000)
+# r3= Product("sabja", 1000)
+
+# Product.helo()
